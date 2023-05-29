8ae607bf9ef5a8a8233f394376fd861c01083cf9
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-05-29 13:13:46 -0300

    delete first test for example

--- src/filter.ts
+++
@@ -1,9 +0,0 @@
-import { contacts } from "./contacts";
-
-const filterByFirstLetterInFullName = (letter: string) => {
-  return contacts.filter((contact) => {
-    return contact.first_name[0] === letter || contact.last_name[0] === letter;
-  });
-};
-
-export { filterByFirstLetterInFullName };
--- tests/filter.test.ts
+++
@@ -1,8 +0,0 @@
-import { filterByFirstLetterInFullName } from "../src/filter";
-
-describe("filterByFirstLetterInFullName", () => {
-  test("deberia devolver 158 contactos que empiezan con la letra M", () => {
-    const contacts = filterByFirstLetterInFullName("M");
-    expect(contacts).toHaveLength(158);
-  });
-});
